8173b5b2888bc75f7cced1e5e8c342d5334487f2
Mihoshi <74931190+Srakawichi@users.noreply.github.com>
2023-06-09 22:26:48 +0200

    Add files via upload

--- germanver.py
+++ germanver.py
@@ -1,8 +1,9 @@
 # Gerüst fertig 08.05.2023
 
-from random import randint
+import random
 from tkinter import *
 list = []
+file_name = "Woerter.txt"
 
 class View:
     def __init__(self, callback):
@@ -26,145 +27,44 @@ class View:
         
 class Model:
     def __init__(self):
-        self.word_list = self.word_list = [
-    "Arbeiterunfallversicherungsgesetz",
-    "Gesetzesentwurf",
-    "Popcorn",
-    "Schmetterling",
-    "Gummibaerchen",
-    "Tischtennis",
-    "Donnerstag",
-    "Zimt",
-    "Krokodil",
-    "Kaktus",
-    "Wolke",
-    "Jeans",
-    "Laub",
-    "Trompete",
-    "Aprikose",
-    "Limonade",
-    "Zebra",
-    "Feuerzeug",
-    "Kiefer",
-    "Mandarine",
-    "Kaese",
-    "Schneeflocke",
-    "Tornado",
-    "Loewe",
-    "Karamell",
-    "Leuchtturm",
-    "Tafel",
-    "Kastanie",
-    "Pirat",
-    "Tukan",
-    "Diamant",
-    "Gletscher",
-    "Spaghetti",
-    "Rhabarber",
-    "Sanduhr",
-    "Kamille",
-    "Computer",
-    "Narzisse",
-    "Waffel",
-    "Rakete",
-    "Schokolade",
-    "Moewe",
-    "Schreibmaschine",
-    "Pflaume",
-    "Briefmarke",
-    "Kaffeemaschine",
-    "Kirschbluete",
-    "Regenbogen",
-    "Palme",
-    "Eule",
-    "Seestern",
-    "Salamander",
-    "Erdbeere",
-    "Perle",
-    "Banane",
-    "Schraubenzieher",
-    "Kaenguru",
-    "Reis",
-    "Maiskolben",
-    "Schnuersenkel",
-    "Edelstein",
-    "Espresso",
-    "Giesskanne",
-    "Windmuehle",
-    "Fuchs",
-    "Ananas",
-    "Honigbiene",
-    "Gurke",
-    "Harfe",
-    "Tuba",
-    "Wasserfall",
-    "Flamingo",
-    "Kaefer",
-    "Koffer",
-    "Kamin",
-    "Floeote",
-    "Pfau",
-    "Bumerang",
-    "Delfin",
-    "Schleife",
-    "Rucksack",
-    "Salbei",
-    "Schildkroete",
-    "Stift",
-    "Taucherbrille",
-    "Zucchini",
-    "Fossil",
-    "Schaufel",
-    "Hubschrauber",
-    "Flieder",
-    "Fledermaus",
-    "Schmetterlingsnetz",
-    "Grill",
-    "Kuckucksuhr",
-    "Holunder",
-    "Zahnrad",
-    "Blume",
-    "Kaefig",
-    "Pinguin",
-    "Schere",
-    "Teekanne",
-    "Kuerbis"
-]
-
-    def randomword(self):
-        word = self.word_list[(randint(0,99))]
-        return word
-    
-    def word_checker(self, word):
+        self.zahl = None
+    #テキストファイルに入った単語を使用   
+    def randomword(self, file_name):
+        file_name = "Woerter.txt"
+        with open(file_name, 'r') as file:
+            words = file.read().split()
+            word = random.choice(words)
+            return word   
+    #入力した単語がテキストファイルに入っているか確認する
+    #出力された単語が入力されていなくてもファイルに入っていれば正解になる
+    def word_checker(self, word, file_name):
+        file_name = "Woerter.txt"
         list.append(word)
-        print(len(list))
-        if word in self.word_list and len(list) != 10:
-            return "Richtig"
-        elif len(list) != 10:
-            return "Falsch"
-        else:
-            return "Geschafft"
-        
-#     def word_counter(self):
-#         if len(list) == 10:
-#             print(len(list))
-        
+        with open(file_name, 'r') as file:
+            words = file.read().split()
+            if word in words and len(list) != 10:
+                return "Richtig"
+            elif len(list) != 10:
+                return "Falsch"
+            else:
+                return "ENDE"
         
 class GController:
     def __init__(self):
         self.model = Model()
         self.view = View(self.callbackKommando)
-        self.view.label.config(text = self.model.randomword())# 同時に違う単語が選べれるのが問題  
+        self.view.label.config(text = self.model.randomword(file_name))# 同時に違う単語が選べれるのが問題  
         self.view.fenster.mainloop()
         
     def callbackKommando(self):
+        file_name = "Woerter.txt"
         #self.model.word_counter()
-        randomword = self.model.randomword()
+        randomword = self.model.randomword(file_name)
         self.view.label.config(text = randomword)
         word = self.view.entry.get()
         word = word.replace(" ","")# スペース入力を無効化
         self.view.entry.delete(0, END)
-        result = self.model.word_checker(word)
+        result = self.model.word_checker(word, file_name)
         self.view.result_label.config(text = result)
         
         if result == "Richtig":
@@ -183,3 +83,4 @@ if __name__ == "__main__":
         
         
         
+
